312b16080b282ae367843f2022bc1ef9d7ab7a95
Utkarsh <utkbits2k11@gmail.com>
2013-05-13 09:36:14 +0200

    Create TRICOUNT.cpp

---
+++ TRICOUNT.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+#include<stdio.h>
+int main()
+{
+    long long int t, N, result;
+    scanf("%lld",&t);
+    while(t--)
+    {
+              scanf("%lld",&N);
+              result=(N*(N+2)*(2*N+1))/8;
+             printf("%lld\n",result);}
+                                     return 0;
+                                     }
+
+
+
+
+
